44eb0fbdb8e977d8088bccf25661d0d21c49f50c
kbrose <kevin@maypark.com>
2013-10-29 16:24:18 -0500

    finished problem 122

--- p120-129/p122.py
+++ p120-129/p122.py
@@ -1,1 +1,24 @@
-    
+class p122:
+    def __init__(self):
+        self.costs = [0] + [200]*200 # the naive way gives lower bound of 200
+        self.path  = [0]*201
+        self.max_k = 200
+
+    def get_costs(self, power, depth):
+        if power > self.max_k or depth > self.costs[power]:
+            return
+        self.costs[power] = depth # new best!
+        self.path[depth] = power # the path has brought us to power
+        for i in range(depth,-1,-1):
+            self.get_costs(power + self.path[i], depth + 1)
+
+    def main(self):
+        self.get_costs(1,0)
+        print sum(self.costs)
+
+    def f(self):
+        print 5
+
+if __name__ == '__main__':
+    p122().main()
+
