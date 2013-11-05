161a0ff3191e942524a348ee009c7fa2d5722b0f
kbrose <kevin@maypark.com>
2013-11-04 20:15:17 -0600

    finished p125.  Eaaassssyyyyy.

--- p120-129/p125.py
+++ p120-129/p125.py
@@ -2,8 +2,16 @@ def is_pal(n):
     s = str(n)
     return s == s[::-1]
 
-pals = []
-for p in range(1001):
-    if is_pal(p):
-        pals.append(p)
-print pals
+def main(n):
+    pals = set()
+    limit = int(n**0.5)+1
+    for start in range(1,limit):
+        for end in range(start+2,limit):
+            test = sum(map (lambda x: x*x, range(start,end)))
+            if test >= n:
+                break
+            if is_pal(test):
+                pals.add(test)
+    return sum(pals)
+
+print main(10**8)
